c974a74e992c2bcc3f84a815a1b70d2e78c9d722
reinhold-b <reinhold.brant@gmx.de>
2025-05-06 20:30:49 +0200

    Init

--- lateral_control.py
+++ lateral_control.py
@@ -6,6 +6,7 @@ class LateralControl:
     def __init__(self):
         self._car_position = np.array([48, 64])
 
-    def control(self):
+    def control(self, trajectory: np.ndarray, speed: np.ndarray) -> float:
+        print(np.subtract(self._car_position, trajectory))
         pass
 
