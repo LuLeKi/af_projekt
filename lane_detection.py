7bf5d57f533f4130f0ca8df488c25a1c49004ac2
reinhold-b <reinhold.brant@gmx.de>
2025-05-06 10:31:47 +0200

    spuren unterscheiden

--- lane_detection.py
+++ lane_detection.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 class LaneDetection:
 
     debug_image = None
-    THRESHOLDING_POINT = 40
+    # for randomize optim calculte histogram and then 
+    # improve contrast
+    THRESHOLDING_POINT = 50
 
     def __init__(self):
         pass
@@ -15,6 +17,31 @@ class LaneDetection:
     def normalize_floats(self, matrix):
         matrix = 255 * (matrix - np.min(matrix)) / (np.max(matrix) - np.min(matrix))
         return matrix
+
+    def binary_dilation(self, img, structure=None):
+        img = img > 0
+        if structure is None:
+            structure = np.ones((3, 3), dtype=bool)
+
+        # Perform convolution
+        convolved = convolve2d(img.view(np.uint8), structure.view(np.uint8), mode='same', boundary='fill', fillvalue=0)
+
+        # Any nonzero result means at least one neighbor was active
+        return convolved > 0
+
+    def grow_region(self, img, start, structure=None):
+        bool_image = img > 0
+        seed = np.zeros_like(bool_image, dtype=bool)
+        seed[start] = True
+
+        prev = None
+        curr = seed.copy()
+
+        while prev is None or not np.array_equal(prev, curr):
+            prev = curr.copy()
+            curr = self.binary_dilation(curr, structure) & bool_image
+ 
+        return curr.astype(np.uint8) * 255
 
 
     def detect(self, state_image):
@@ -45,17 +72,29 @@ class LaneDetection:
 
         # thresholding
         grad = (grad > self.THRESHOLDING_POINT) * grad * (255 / grad)
- 
 
-        # print(cx, cy)
+        left_x, right_x = -1, -1
+        edges = np.where(grad[cary] > 0)[0]
+        if len(edges) >= 2:
+            left_x = edges[0]     # leftmost
+            right_x = edges[-1]    # rightmost
+        else:
+            # fallback if not enough edges
+            left_x, right_x = -1, -1
+
+        left_lane = self.grow_region(grad, (cary, left_x), structure=np.ones((3, 3), dtype=bool))
+        right_lane = self.grow_region(grad, (cary, right_x), structure=np.ones((3, 3), dtype=bool))
 
         # rescale in the end
         # gray_normalized = grad.astype(np.uint8)
 
         # plt.imshow(gray_normalized, cmap='jet')
-        # plt.colorbar()  
+        # plt.colorbar()
         # mplt.title("Gradient Magnitude with Colormap")
         # plt.show()
-        self.debug_image = grad 
-       # self.debug_image = state_image
+        left_lane = np.stack((left_lane,) * 3, axis=-1) * (100, 98, 200) 
+        right_lane = np.stack((right_lane,) * 3, axis=-1) * (50, 200, 180) 
+        self.debug_image = left_lane + right_lane 
+        print(self.debug_image)
+      # self.debug_image = state_image
         pass
